8385b316ed394a8646541ae7bf9133f968aa3d3a
ramosjaelynn3-cyber <ramosjaelynn3@gmail.com>
2026-05-01 12:54:00 -0700

    Update conditions.js

--- systems/conditions.js
+++ systems/conditions.js
@@ -1,1 +1,35 @@
+import { getStat } from "./stats.js";
+import { hasItem } from "./inventory.js";
 
+export function evaluateCondition(player, condition) {
+  if (!condition) return true;
+
+  // AND
+  if (condition.and) {
+    return condition.and.every(c => evaluateCondition(player, c));
+  }
+
+  // OR
+  if (condition.or) {
+    return condition.or.some(c => evaluateCondition(player, c));
+  }
+
+  // NOT
+  if (condition.not) {
+    return !evaluateCondition(player, condition.not);
+  }
+
+  // STAT CHECK
+  if (condition.stat) {
+    return Object.entries(condition.stat).every(([stat, value]) => {
+      return getStat(player, stat) >= value;
+    });
+  }
+
+  // ITEM CHECK
+  if (condition.item) {
+    return hasItem(player, condition.item);
+  }
+
+  return true;
+}
