10d58cc664bd2b5a9790c2ec2d8703cf6a8071d2
ramosjaelynn3-cyber <ramosjaelynn3@gmail.com>
2026-05-01 12:42:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+import { scenes } from "./data/scenes.js";
+import { player } from "./data/player.js";
 
+let currentScene = "intro";
+
+const sceneText = document.getElementById("scene-text");
+const choicesBox = document.getElementById("choices");
+
+function renderScene(sceneId) {
+  const scene = scenes[sceneId];
+  currentScene = sceneId;
+
+  // text
+  sceneText.innerText = scene.text;
+
+  // choices
+  choicesBox.innerHTML = "";
+
+  scene.choices.forEach(choice => {
+    const btn = document.createElement("button");
+    btn.innerText = choice.text;
+
+    btn.onclick = () => {
+      if (choice.effect) choice.effect(player);
+      renderScene(choice.next);
+    };
+
+    choicesBox.appendChild(btn);
+  });
+}
+
+// start game
+renderScene(currentScene);
